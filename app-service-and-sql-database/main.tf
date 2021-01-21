7c881bf10ce2e3e94d6555a4bcaedb7f16200b7c
sivakaruppiah <44968911+sivakaruppiah@users.noreply.github.com>
2021-01-21 18:40:37 +0000

    Update main.tf

--- app-service-and-sql-database/main.tf
+++ app-service-and-sql-database/main.tf
@@ -1,8 +1,6 @@
 provider "azurerm" {
-    # The "feature" block is required for AzureRM provider 2.x. 
-    # If you're southing version 1.x, the "features" block is not allowed.
-    version = "~>2.0"
-    features {}
+  features {}
+}
 
   # More information on the authentication methods supported by
   # the AzureRM Provider can be found here:
