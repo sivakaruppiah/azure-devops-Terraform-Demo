bf2e86f576be799ff6434599bd9ea67f69ecd7e9
sivakaruppiah <44968911+sivakaruppiah@users.noreply.github.com>
2021-01-21 21:48:24 +0000

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -15,36 +15,6 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "RG-Terraform" {
-  name     = "terraform-resource-group"
-  location = "West Europe"
-}
-
-resource "azurerm_app_service_plan" "ASP-TerraForm" {
-  name                = "terraform-appserviceplan"
-  location            = azurerm_resource_group.RG-Terraform.location
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
-}
-
-resource "azurerm_app_service" "AS-Terraform" {
-  name                = "app-service-terraform"
-  location            = azurerm_resource_group.RG-Terraform.location
-  resource_group_name = azurerm_resource_group.RG-Terraform.name
-  app_service_plan_id = azurerm_app_service_plan.ASP-TerraForm.id
-
-  site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
-  }
-
-  app_settings = {
-    "SOME_KEY" = "some-value"
-  }
 
   connection_string {
     name  = "Database"
