8f3d5d8ad0d16614fc4c3069a9442a03264158e3
sivakaruppiah <44968911+sivakaruppiah@users.noreply.github.com>
2021-01-21 17:53:15 +0000

    Update main.tf

--- app-service-and-sql-database-var/main.tf
+++ app-service-and-sql-database-var/main.tf
@@ -1,3 +1,20 @@
+provider "azurerm" {
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you're southing version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+
+  # More information on the authentication methods supported by
+  # the AzureRM Provider can be found here:
+  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs
+
+  # subscription_id = "..."
+  # client_id       = "..."
+  # client_secret   = "..."
+  # tenant_id       = "..."  
+}
+
+
 resource "azurerm_resource_group" "RG-Terraform" {
   name     = "terraform-resource-group"
   location = "West Europe"
